89d70411acf3d75875f7a0c2a66bf79ce7590795
Eric Weischedel <eweischedel@scheduleengine.com>
2020-06-24 16:47:57 -0400

    Rename login to request token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -24,17 +24,10 @@ interface RegisterInput {
 @Tags('Auth')
 export class UserController extends Controller {
 
-  @Post('/login')
+  @Post('/request-token')
   public async login(@Body() requestBody: LoginInput): Promise<LoginOutput> {
     return {
       authToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-  }
-
-  @Post('/logout')
-  public async logout(@Query() authToken: string): Promise<LogoutOutput> {
-    return {
-      message: "Successfully logged out."
     }
   }
 
