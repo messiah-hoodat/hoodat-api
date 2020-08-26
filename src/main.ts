6344e69e8a3f2aa3118201351d438e0f8f925870
Eric Weischedel <eweischedel@scheduleengine.com>
2020-08-26 16:45:56 -0400

    Modify mongoose connection

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,33 @@
 import * as express from 'express';
-import * as swaggerUi from 'swagger-ui-express';
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
+import * as swaggerUi from 'swagger-ui-express';
 
 import { RegisterRoutes } from './routes/routes';
 
 dotenv.config();
 
+const { DB_CONNECTION_STRING, PORT } = process.env;
+
 const app = express();
 app.use(express.json())
-const port = process.env.PORT || 8000;
 
-const connectionString = process.env.DB_CONNECTION_STRING;
-
-mongoose.connect(connectionString, (err) => {
-  if (err) {
-    console.log(err);
-    throw new Error(err.errmsg);
+mongoose.connect(
+  DB_CONNECTION_STRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+      throw new Error(err.errmsg);
+    }
+    console.log('Connected to database.');
   }
-  console.log('Connected to database!')
-});
-
+);
 
 RegisterRoutes(app);
 
 const swaggerDocument = require('../api/dist/swagger.json');
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(port, () => console.log(`Server started listening to port ${port}`));
+const port = PORT || 8000;
+app.listen(port, () => console.log(`Server started listening to port ${port}.`));
