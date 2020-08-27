d124244bc34cdf19fc4e82ccc90996ec418bf309
Eric Weischedel <eweischedel@scheduleengine.com>
2020-08-27 19:00:04 -0400

    Modify signUpInputSchema

--- src/schemas/signUpInputSchema.ts
+++ src/schemas/signUpInputSchema.ts
@@ -1,14 +1,13 @@
 import * as Joi from 'joi';
 
 export const signUpInputSchema = Joi.object({
-  name: Joi
-    .string()
-    .alphanum()
+  name: Joi.string()
+    .pattern(/^[a-zA-Z ,.'-]+$/i)
     .min(3)
     .max(50)
     .required(),
-  username: Joi
-    .string()
+  username: Joi.string()
+    .alphanum()
     .min(3)
     .max(50)
     .required(),
@@ -16,4 +15,5 @@ export const signUpInputSchema = Joi.object({
     .string()
     .min(8)
     .max(200)
+    .required()
 });
