806d1d851338fc45f8d420a8d2d9340850561259
Eric Weischedel <eweischedel@scheduleengine.com>
2020-08-28 19:17:43 -0400

    Begin work on get user by id

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,29 +1,30 @@
-import * as express from "express";
-import * as jwt from "jsonwebtoken";
+import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as Boom from '@hapi/boom';
 
 export function expressAuthentication(
   request: express.Request,
   securityName: string,
   scopes?: string[]
 ): Promise<any> {
-if (securityName === "jwt") {
+if (securityName === 'jwt') {
     const token =
       request.body.token ||
       request.query.token ||
-      request.headers["x-access-token"];
+      request.headers['x-access-token'];
 
     return new Promise((resolve, reject) => {
       if (!token) {
-        reject(new Error("No token provided"));
+        reject(Boom.forbidden('No token provided'));
       }
       jwt.verify(token, process.env.TOKEN_SECRET, function (err: any, decoded: any) {
         if (err) {
-          reject(err);
+          reject(Boom.forbidden('Token verification error', err));
         } else {
           // Check if JWT contains all required scopes
           for (let scope of scopes) {
             if (!decoded.scopes.includes(scope)) {
-              reject(new Error("JWT does not contain required scope."));
+              reject(Boom.forbidden('Token does not contain required scope', scope));
             }
           }
           resolve(decoded);
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Get, Route, Tags, Query, Body, Path, Security } from 'tsoa';
+import * as jwt from 'jsonwebtoken';
+import * as Boom from '@hapi/boom';
+import { Controller, Post, Get, Route, Tags, Query, Body, Path, Security, Header } from 'tsoa';
 
 import { User } from '../models/User';
 
 interface UserOutput {
-  email: string,
-  firstName: string,
-  lastName: string
+  username: string,
+  name: string,
 }
 
 @Route('/users')
@@ -27,15 +28,16 @@ export class UserController {
   }
 
   /**
-   * Gets a specific user by user id
+   * Gets a specific user by user ID
    */
   @Security('jwt')
   @Get('{userId}')
-  public async getUser(@Path() userId: string): Promise<UserOutput> {
-    return {
-      email: 'johndoe@gmail.com',
-      firstName: 'John',
-      lastName: 'Doe',
-    };
+  public async getUser(@Path() userId: string, @Header('x-access-token') token: string): Promise<any> {
+    const decoded = jwt.decode(token, { json: true });
+    const tokenId = decoded.userId;
+    if (tokenId !== userId) {
+      throw Boom.badRequest('User ID in path does not match user ID in token')
+    }
+    return decoded;
   }
 }
