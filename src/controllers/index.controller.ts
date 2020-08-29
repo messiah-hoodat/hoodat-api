5179f4c0125ad12d7a77839fc2530527dc0b979e
Eric Weischedel <eweischedel@scheduleengine.com>
2020-08-29 11:51:23 -0400

    Health Checks -> Status

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, Route, Tags } from 'tsoa';
 
 @Route('')
-@Tags('Health Checks')
+@Tags('Status')
 export class IndexController extends Controller {
+
+  /**
+  *  Returns status information about the service
+  */
   @Get('')
   public async index() {
       return { msg: 'The service is up and running!' };
