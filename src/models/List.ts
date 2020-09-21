82e0da7fa02916aae37819631a70d8d5828c387a
Eric Weischedel <eweischedel@scheduleengine.com>
2020-09-21 15:31:15 -0400

    Add list model

---
+++ src/models/List.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+export interface ListDocument extends mongoose.Document {
+  name: string;
+  owner: string;
+  contacts: string[];
+}
+
+export const ListSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  contacts: {
+    type: [String],
+    required: false
+  }
+});
+
+export const List = mongoose.model('lists', ListSchema);
