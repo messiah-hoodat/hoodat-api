c153aed1b885d150868a0945e647016235984d9c
Eric Weischedel <eweischedel@scheduleengine.com>
2020-09-21 19:31:16 -0400

    fix format

--- src/lib/getDecodedToken.ts
+++ src/lib/getDecodedToken.ts
@@ -4,7 +4,7 @@ interface Token {
   userId: string;
 }
 
-export default function getDecodedToken(authHeader: string): Token  {
+export default function getDecodedToken(authHeader: string): Token {
   const token = authHeader.split('Bearer ')[1];
   const decoded = jwt.decode(token, { json: true });
   return decoded as Token;
